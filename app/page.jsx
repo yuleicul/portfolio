1711d0db4144c0f40a2a1a35424974663faf7755
Yulei Chen <chenyuleiw@gmail.com>
2024-03-27 00:06:38 +0800

    Set up unocss

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="...">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold text-red-800">
+            src/app/page.tsx
+          </code>
+        </p>
+      </div>
+    </main>
+  );
+}
